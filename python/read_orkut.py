0df8402543bccb094b3c00ed588bab5b83a2c5ac
David Wiebe <dlw13@sfu.ca>
2025-01-14 11:57:54 -0800

    Clean read_orkut.py and replace sections with networkx calls for simplicity and performance

--- python/read_orkut.py
+++ python/read_orkut.py
@@ -2,45 +2,26 @@
 import csv
 import numpy as np
 import re
+import networkx as nx
 from scipy.sparse import lil_matrix
+from common import log
 
 
-def read_orkut(orkut_file: str, edge_file: str, degree_file: str):
+def read_orkut(input_file: str, edge_file: str, degree_file: str):
     # Read max_user_ID from the Orkut file --> max_user_id
-    f = open(orkut_file, "r")
-    max_user_id = 0
-    pattern = re.compile(r"([0-9]+).*([0-9]+)")
-    for i, line in enumerate(f):
-        # Skip the header
-        if i < 4:
-            continue
-        else:
-            lst = pattern.findall(line)[0]
-            user1 = int(lst[0]) - 1
-            user2 = int(lst[1]) - 1
-            if max_user_id < user1:
-                max_user_id = user1
-            if max_user_id < user2:
-                max_user_id = user2
-    f.close()
+    graph: nx.Graph = nx.read_edgelist(input_file, nodetype=int)
+    max_user_id = max(graph.nodes)
     user_num = max_user_id + 1
     edges_lil = lil_matrix((user_num, user_num))
     deg = np.zeros(user_num)
 
     # Read edges and degrees from the Orkut file --> edges_lil, deg
-    f = open(orkut_file, "r")
-    for i, line in enumerate(f):
-        # Skip the header
-        if i < 4:
-            continue
-        else:
-            lst = pattern.findall(line)[0]
-            user1 = int(lst[0]) - 1
-            user2 = int(lst[1]) - 1
-            edges_lil[user1, user2] = 1
-            deg[user1] += 1
-            deg[user2] += 1
-    f.close()
+    for edge in graph.edges:
+        user1 = edge[0] - 1
+        user2 = edge[1] - 1
+        edges_lil[user1, user2] = 1
+        deg[user1] += 1
+        deg[user2] += 1
 
     # Extract users with deg >= 1 and create new user IDs --> user_dic ({user_id, new_user_id})
     user_dic = {}
@@ -49,42 +30,38 @@ def read_orkut(orkut_file: str, edge_file: str, degree_file: str):
         if deg[user_id] > 0:
             user_dic[user_id] = new_user_id
             new_user_id += 1
-    print("#users:", len(user_dic))
+    log(f"#users: {len(user_dic)}")
 
-    # edges_csr = edges_lil.tocsr()
     a1, a2 = edges_lil.nonzero()
-    # a1, a2 = edges_csr.nonzero()
-    print("#edges:", len(a1))
+    log(f"#edges: {len(a1)}")
 
     # Output edge information
-    print("Outputting edge information.")
-    f = open(edge_file, "w")
-    print("#nodes", file=f)
-    print(len(user_dic), file=f)
-    print("node,node", file=f)
-    writer = csv.writer(f, lineterminator="\n")
-    for i in range(len(a1)):
-        # user_ids --> user_id1, user_id2
-        user_id1 = a1[i]
-        user_id2 = a2[i]
-        # new_user_ids --> user1, user2
-        user1 = user_dic[user_id1]
-        user2 = user_dic[user_id2]
-        lst = [user1, user2]
-        writer.writerow(lst)
-    f.close()
+    log("Outputting edge information")
+    with open(edge_file, "w") as edge_handle:
+        print("#nodes", file=edge_handle)
+        print(len(user_dic), file=edge_handle)
+        print("node,node", file=edge_handle)
+        writer = csv.writer(edge_handle, lineterminator="\n")
+        for i in range(len(a1)):
+            # user_ids --> user_id1, user_id2
+            user_id1 = a1[i]
+            user_id2 = a2[i]
+            # new_user_ids --> user1, user2
+            user1 = user_dic[user_id1]
+            user2 = user_dic[user_id2]
+            edge = [user1, user2]
+            writer.writerow(edge)
 
     # Output degree information
     print("Outputting degree information.")
-    f = open(degree_file, "w")
-    print("node,deg", file=f)
-    writer = csv.writer(f, lineterminator="\n")
-    for user_id in range(user_num):
-        if deg[user_id] == 0:
-            continue
-        # new_user_id --> user1
-        user1 = user_dic[user_id]
-        # new_user_id and her deg --> lst
-        lst = [user1, int(deg[user_id])]
-        writer.writerow(lst)
-    f.close()
+    with open(degree_file, "w") as degree_handle:
+        print("node,deg", file=degree_handle)
+        writer = csv.writer(degree_handle, lineterminator="\n")
+        for user_id in range(user_num):
+            if deg[user_id] == 0:
+                continue
+            # new_user_id --> user1
+            user1 = user_dic[user_id]
+            # new_user_id and her deg --> lst
+            lst = [user1, int(deg[user_id])]
+            writer.writerow(lst)
