a8925e34eaa385cb055d33ba7fabd340bd1f847a
David Wiebe <dlw13@sfu.ca>
2025-01-14 15:51:19 -0800

    Expand logic in main.py

--- python/main.py
+++ python/main.py
@@ -1,6 +1,6 @@
 import subprocess
-import sys
 import os
+import csv
 from read_orkut import read_orkut
 from ba_graph import ba_graph
 from common import *
@@ -16,10 +16,47 @@ if __name__ == "__main__":
     replications: int = args.replications
     disable: bool = args.disable
 
-    read_orkut(input_file, "../data/Orkut/edges.csv", "../data/Orkut/deg.csv")
-    ba_graph(
-        1000000, 10, "../data/BAGraph-m10/edges.csv", "../data/BAGraph-m10/deg.csv"
-    )
+    if disable:
+        log("Disabled by user input")
+        log()
+        alert = "disabled"
+        write_results(output_file, args, alert)
+        status = 0
+        log(f"********** Exit Status {status} **********")
+        exit({status})
 
-    os.chdir("..")
-    subprocess.run(["./run_EvalLossLocal.sh", "Orkut"])
+    log("Running edge table and degree distribution setup")
+    edge_file = "../data/Orkut/edges.csv"
+    degree_file = "../data/Orkut/deg.csv"
+    read_orkut(input_file, edge_file, degree_file)
+    log("Running edge table and degree distribution setup - Done")
+    log()
+
+    log("Creating Barabasi Albert graph")
+    n_nodes = 1000000
+    n_edges_on_new_node = 10
+    edge_file = f"../data/BAGraph-m{n_edges_on_new_node}/edges.csv"
+    degree_file = f"../data/BAGraph-m{n_edges_on_new_node}/deg.csv"
+    ba_graph(n_nodes, n_edges_on_new_node, edge_file, degree_file)
+    log("Creating Barabasi Albert graph - Done")
+    log()
+
+    log("Running statistics calculations")
+    n = 100
+    noise_type = 1
+    algorithm = 0
+    print("", flush=True)
+    os.chdir("../cpp")
+    statistics_calculation_args = [
+        "./EvalLossLocal",
+        "Orkut",
+        f"{n}",
+        f"{epsilon}",
+        f"{noise_type}",
+        f"{replications}",
+        f"{algorithm}",
+    ]
+    subprocess.run(statistics_calculation_args)
+    os.chdir("../python")
+    log("Running statistics calculations - Done")
+    log()
